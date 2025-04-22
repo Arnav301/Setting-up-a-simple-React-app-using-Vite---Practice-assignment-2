865882f983e68f21b66b2ff974834c11e9e48e77
Arnav Verma <arnavverma3016@gmail.com>
2025-04-22 10:50:15 +0530

    Updated code

---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="contact-details">
+            <h1>Name: Arnav Verma</h1>
+            <h1>contact number: 90414xxxxx</h1>
+            <h1>Gmail address: Arnav@gmail.com</h1>
+        </div>
+    )
+}
+
+export default Home;
